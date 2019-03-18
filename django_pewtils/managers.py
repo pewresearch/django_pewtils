fcc8b970febc7bfa7b2e75fa08f54c53021179d5
pvankessel <pvankessel@pewresearch.org>
2019-03-18 11:49:52 -0400

    removed future standard_library import

--- django_pewtils/managers.py
+++ django_pewtils/managers.py
@@ -1,6 +1,5 @@
 from __future__ import print_function
-from future import standard_library
-standard_library.install_aliases()
+
 from builtins import str
 import traceback, sys, pandas, os, hashlib, random, psycopg2
 
