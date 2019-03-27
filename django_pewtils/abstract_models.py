5e44fa26e7f47ee4dd4cbc429797a24f60766b9a
pvankessel <pvankessel@pewresearch.org>
2019-03-27 15:35:47 -0400

    modified the similar_by functions to actually be useful when called on objects

--- django_pewtils/abstract_models.py
+++ django_pewtils/abstract_models.py
@@ -39,45 +39,84 @@ class BasicExtendedModel(models.Model):
 
     def fuzzy_ratio(self, field_names, text, allow_partial=False, max_partial_difference=100):
 
-        return get_model(self._meta.model_name).objects.filter(pk=self.pk).fuzzy_ratios(field_names, text,
-                                                                                        allow_partial=allow_partial,
-                                                                                        max_partial_difference=max_partial_difference)[
-            0]['fuzzy_ratio']
+        return get_model(self._meta.model_name).objects\
+            .filter(pk=self.pk)\
+            .fuzzy_ratios(
+                field_names,
+                text,
+                allow_partial=allow_partial,
+                max_partial_difference=max_partial_difference
+        )[0]['fuzzy_ratio']
 
-    def similar_by_fuzzy_ratios(self, field_names, text, min_ratio=90, allow_partial=False, max_partial_difference=10):
+    def similar_by_fuzzy_ratios(self, field_names, min_ratio=90, allow_partial=False, max_partial_difference=10):
 
-        return get_model(self._meta.model_name).objects.exclude(pk=self.pk).fuzzy_ratios(field_names, text,
-                                                                                         min_ratio=min_ratio,
-                                                                                         allow_partial=allow_partial,
-                                                                                         max_partial_difference=max_partial_difference)
+        text = " ".join([decode_text(getattr(self, f)) for f in field_names if getattr(self, f)])
+        return get_model(self._meta.model_name).objects\
+            .exclude(pk=self.pk)\
+            .fuzzy_ratios(
+                field_names,
+                text,
+                min_ratio=min_ratio,
+                allow_partial=allow_partial,
+                max_partial_difference=max_partial_difference
+        )
 
     def levenshtein_difference(self, field_names, text):
 
-        return \
-        get_model(self._meta.model_name).objects.filter(pk=self.pk).levenshtein_differences(field_names, text)[0][
-            'difference']
+        return get_model(self._meta.model_name).objects\
+            .filter(pk=self.pk)\
+            .levenshtein_differences(
+                field_names,
+                text
+            )[0]['difference']
 
-    def similar_by_levenshtein_differences(self, field_names, text, max_difference=.2):
+    def similar_by_levenshtein_differences(self, field_names, max_difference=.2):
 
-        return get_model(self._meta.model_name).objects.exclude(pk=self.pk).levenshtein_differences(field_names, text,
-                                                                                                    max_difference=max_difference)
+        text = " ".join([decode_text(getattr(self, f)) for f in field_names if getattr(self, f)])
+        return get_model(self._meta.model_name).objects\
+            .exclude(pk=self.pk)\
+            .levenshtein_differences(
+                field_names,
+                text,
+                max_difference=max_difference
+        )
 
     def tfidf_similarity(self, field_names, text):
 
-        return get_model(self._meta.model_name).objects.filter(pk=self.pk).tfidf_similarities(field_names, text)[0][
-            'similarity']
+        return get_model(self._meta.model_name).objects\
+            .filter(pk=self.pk)\
+            .tfidf_similarities(
+                field_names,
+                text
+        )[0]['similarity']
 
-    def similar_by_tfidf_similarity(self, field_names, text, min_similarity=.9):
+    def similar_by_tfidf_similarity(self, field_names, min_similarity=.9):
 
-        return get_model(self._meta.model_name).objects.exclude(pk=self.pk).tfidf_similarities(field_names, text,
-                                                                                               min_similarity=min_similarity)
+        text = " ".join([decode_text(getattr(self, f)) for f in field_names if getattr(self, f)])
+        return get_model(self._meta.model_name).objects\
+            .exclude(pk=self.pk)\
+            .tfidf_similarities(
+                field_names,
+                text,
+                min_similarity=min_similarity
+        )
 
     def trigram_similarity(self, field_names, text):
 
-        return get_model(self._meta.model_name).objects.filter(pk=self.pk).trigram_similarities(field_names, text)[0][
-            'similarity']
+        return get_model(self._meta.model_name).objects\
+            .filter(pk=self.pk)\
+            .trigram_similarities(
+                field_names,
+                text
+        )[0]['similarity']
 
-    def similar_by_trigram_similarity(self, field_names, text, min_similarity=.9):
+    def similar_by_trigram_similarity(self, field_names, min_similarity=.9):
 
-        return get_model(self._meta.model_name).objects.exclude(pk=self.pk).trigram_similarities(field_names, text,
-                                                                                                 min_similarity=min_similarity)
+        text = " ".join([decode_text(getattr(self, f)) for f in field_names if getattr(self, f)])
+        return get_model(self._meta.model_name).objects\
+            .exclude(pk=self.pk)\
+            .trigram_similarities(
+                field_names,
+                text,
+                min_similarity=min_similarity
+        )
