b60cba571ecd2bf8dea61a97b583d1445741a9c1
Brian Broderick <brian@broderick.io>
2021-09-27 15:56:26 +0000

    add bumpversion config, update setup.py

--- setup.py
+++ setup.py
@@ -1,42 +1,36 @@
-from distutils.core import setup
-from setuptools import find_packages
+import os
+from setuptools import setup, find_packages
 
 
-with open("README.md") as README:
-    readme = str(README.read())
+with open(os.path.join(os.path.dirname(__file__), "README.md"), "rb") as readme:
+    README = str(readme.read())
+
+os.chdir(os.path.normpath(os.path.join(os.path.abspath(__file__), os.pardir)))
 
 install_requires = []
 with open("requirements.txt") as reqs:
     for line in reqs.read().split("\n"):
-        if not line.startswith("#"):
+        if line and not line.startswith(("#", "--", "git+ssh")):
             install_requires.append(line)
 
 setup(
-    name="django_pewtiils",
-    version="1.0.0",
-    description="Miscellaneous Django utilities from Pew Research Center",
-    long_description=readme,
+    name="django_pewtils",
+    version='0.1.4.dev0',
+    description="miscellaneous utilities",
+    long_description=README,  # 'http://labs.pewresearch.tech/docs/libs/django_pewtils',
     url="https://github.com/pewresearch/django_pewtils",
     author="Pew Research Center",
-    author_email="info@pewresearch.org",
+    author_email="admin@pewresearch.tech",
     install_requires=install_requires,
     packages=find_packages(exclude=["contrib", "docs", "tests"]),
     include_package_data=True,
-    keywords="django, pew pew pew",
-    license="GPLv2+",
     classifiers=[
         # https://pypi.python.org/pypi?%3Aaction=list_classifiers
         "Development Status :: 5 - Production/Stable",
-        "Environment :: Console",
-        "Intended Audience :: Information Technology",
-        "Intended Audience :: Developers",
-        "License :: OSI Approved :: GNU General Public License v2 or later (GPLv2+)",
-        "Natural Language :: English",
-        "Operating System :: OS Independent",
-        "Programming Language :: Python :: 2.7",
-        "Programming Language :: Python :: 3.6",
-        "Programming Language :: Python :: 3.7",
-        "Programming Language :: Python :: 3.8",
+        #        'Development Status :: 6 - Mature',
+        #        'Development Status :: 7 - Inactive'
+        "Programming Language :: Python :: 3.9",
+        "Intended Audience :: Science/Research",
         "Topic :: Software Development :: Libraries :: Python Modules",
         "Topic :: Utilities",
     ],
