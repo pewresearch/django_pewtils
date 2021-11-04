e335b8b2a30a56604af75d44b2ea6586b3478fd5
Brian Broderick <brian@broderick.io>
2021-11-04 20:46:49 +0000

    Bump version: 0.2.0.dev0 → 0.2.0

--- setup.py
+++ setup.py
@@ -15,7 +15,7 @@ with open("requirements.txt") as reqs:
 
 setup(
     name="django_pewtils",
-    version="0.2.0.dev0",
+    version="0.2.0",
     description="miscellaneous utilities",
     long_description=README,  # 'http://labs.pewresearch.tech/docs/libs/django_pewtils',
     url="https://github.com/pewresearch/django_pewtils",
